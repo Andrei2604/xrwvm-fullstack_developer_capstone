88761f91e6e050f8844e4fe5157ff1d0906e48bc
Andrei <andrejporodin@gmail.com>
2024-10-09 14:30:59 +1000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -15,6 +14,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    
     def __str__(self):
         return self.name
 
@@ -29,18 +29,19 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete = models.CASCADE)
-    name = models.CharField(max_length = 100)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'Suv'),
         ('WAGON', 'Wagon'),
     ]
-    type = models.CharField(max_length = 10, choices = CAR_TYPES, default = 'SUV')
-    year = models.IntegerField(default = 2024,
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2024,
         validators = [
             MaxValueValidator(2024),
             MinValueValidator(2015)
         ])
+    
     def __str__(self):
         return self.name
